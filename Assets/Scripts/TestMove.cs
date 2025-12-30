8bbae8e0ea85897cca9a0dbe8cd0c9ce11ac1cc6
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-12-30 17:42:03 +0900

    20251230_Commit

--- Assets/Scripts/TestMove.cs
+++ Assets/Scripts/TestMove.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 namespace I_Walk
 {
-    public class MoveTest : MonoBehaviour
+    public class TestMove : MonoBehaviour
     {
+        Animator _anim;
+
+        [SerializeField] float _speed = 0;
+
+        float _horizontal;
+        float _vertical;
+
+        Vector3 move;
+        Vector3 _lookDirection;
+
+
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            _anim = GetComponent<Animator>();
         }
 
         // Update is called once per frame
