cf1cbf55b10e8af65f58b98baefc118b41fe1031
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-12-31 16:20:26 +0900

    20251231_Commit

--- Assets/Scripts/TestMove.cs
+++ Assets/Scripts/TestMove.cs
@@ -12,6 +12,9 @@ namespace I_Walk
         float _horizontal;
         float _vertical;
 
+        Vector3 _move;
+        Vector3 _lookDirection;
+
         void Start()
         {
             _anim = GetComponent<Animator>();
@@ -23,24 +26,7 @@ namespace I_Walk
             _horizontal = Input.GetAxis("Horizontal");
             _vertical = Input.GetAxis("Vertical");
 
-            // 2. 이동 방향 계산
-            Vector3 moveDir = new Vector3(_horizontal, 0, _vertical).normalized;
 
-            // 3. 애니메이션 Speed 파라미터 전달
-            // .magnitude를 쓰면 대각선 이동 시에도 적절한 속도값(0~1 사이)이 전달됩니다.
-            float moveMagnitude = moveDir.magnitude;
-            _anim.SetFloat("Speed", moveMagnitude);
-
-            // 4. 실제로 이동 및 회전 처리
-            if (moveMagnitude > 0.1f)
-            {
-                // 이동
-                transform.position += moveDir * _moveSpeed * Time.deltaTime;
-
-                // 캐릭터가 이동 방향을 부드럽게 바라보게 함
-                Quaternion targetRotation = Quaternion.LookRotation(moveDir);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _rotateSpeed * Time.deltaTime);
-            }
         }
     }
 }
