d3a80717a23fc69a487ac8a1c8826e1a14fea162
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-01-26 19:16:42 +0900

    20260126_Commit

---
+++ Assets/Scripts/Player/Animation/Pakour/PakourAnimation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace I_Walk
+{
+    public class PakourAnimation : MonoBehaviour
+    {
+        [SerializeField] float _widthGizmo = 0;
+        [SerializeField] float _hightGizmo = 0;
+
+        bool _isGizmoIn = false;
+
+
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+
+        private void OnDrawGizmos()
+        {
+            
+        }
+    }
+}
