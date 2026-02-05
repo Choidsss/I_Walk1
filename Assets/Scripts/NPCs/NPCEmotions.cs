fb18000be1847ee85faa7c2fb26f25491d30ce9e
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-02-05 16:09:06 +0900

    20260205_Commit

---
+++ Assets/Scripts/NPCs/NPCEmotions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace I_Walk
+{
+    public class NPCEmotions : MonoBehaviour
+    {
+        //bool _isDialoguePrinted = false;
+
+        //public bool IsDialoguePrinted { get { return _isDialoguePrinted; } set { _isDialoguePrinted = value; } }
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
