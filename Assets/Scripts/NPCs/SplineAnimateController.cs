e8958e9428151057a4fe1f9448af3b6afb7a18bc
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-02-26 18:22:56 +0900

    20260226_Commit

---
+++ Assets/Scripts/NPCs/SplineAnimateController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Splines;
+using System.Collections.Generic;
+using TMPro;
+
+namespace I_Walk
+{
+    public class SplineAnimateController : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _tmpUGUI;
+        
+        [Header("PlayKeyward")]
+        [SerializeField] string _playKeyward;
+        
+        Animator _anim;
+        SplineAnimate _splineAnimate;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _anim = GetComponent<Animator>();
+            _splineAnimate = GetComponent<SplineAnimate>();
+            _tmpUGUI = GetComponent<TextMeshProUGUI>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_tmpUGUI != null && _tmpUGUI.text == _playKeyward)
+            {
+                SplineAnimationPlayOn();
+            }
+        }
+
+        private void SplineAnimationPlayOn()
+        {
+            //ToDo : 딜레이 3초 주기
+            _splineAnimate.Play();
+            _anim.SetBool("Running", true);
+        }
+
+        private void SplineAnimationEnded()
+        {
+            _anim.SetBool("Running", false);
+        }
+
+        //IEnumerator DelayAnimation()
+        //{
+        //    yield return 1;
+        //}
+    }
+}
