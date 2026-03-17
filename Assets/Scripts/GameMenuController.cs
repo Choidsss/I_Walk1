d2bc4af04316bfffd4a3fff4d6aeb1f89f84797f
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-03-17 16:45:21 +0900

    20260317_Commit

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,15 +13,41 @@ namespace I_Walk
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            _uiPanel.SetActive(false);
-            _startButton.enabled = false;
-            _endButton.enabled = false;
+            _uiPanel.SetActive(true);
+
+            //버튼이 눌리기 전까지는 게임 정지
+            Time.timeScale = 0;
+
+            _startButton.onClick.AddListener(ClickedStartButton);
+            _endButton.onClick.AddListener(ClickedEndButton);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void ClickedStartButton()
         {
-        
+            _uiPanel.SetActive(false);
+
+            Time.timeScale = 1.0f;
+        }
+
+        private void ClickedEndButton()
+        {
+            _uiPanel.SetActive(false);
+
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            Debug.Log("Quit Game");
+#else
+     Application.Quit();
+
+#endif
+        }
+
+
+        private void EndingSceneAfter()
+        {
+            //엔딩이 나오고 나면 일정시간이 지나고 메뉴창 켜기
+
         }
     }
 }
