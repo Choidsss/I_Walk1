842a401b00e9a01bcbb510580825408bff7dd4d0
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-03-17 17:35:14 +0900

    20260317_Commit

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -1,22 +1,33 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Cinemachine;
 
 namespace I_Walk
 {
     public class GameMenuController : MonoBehaviour
     {
+        [SerializeField] GameObject _uiPanel;
         [SerializeField] Button _startButton;
         [SerializeField] Button _endButton;
-        [SerializeField] GameObject _uiPanel;
+        [SerializeField] CinemachineCamera _startCam;
+        [SerializeField] CinemachineCamera _freeLookCam;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Awake()
+        {
+            _startCam.Priority = 100;
+            _freeLookCam.Priority = 30;
+
+            _startCam.enabled = true;
+            _freeLookCam.enabled = false;
+        }
+
         void Start()
         {
-            _uiPanel.SetActive(true);
-
             //버튼이 눌리기 전까지는 게임 정지
             Time.timeScale = 0;
+
+            _uiPanel.SetActive(true);
 
             _startButton.onClick.AddListener(ClickedStartButton);
             _endButton.onClick.AddListener(ClickedEndButton);
@@ -24,8 +35,12 @@ namespace I_Walk
 
         private void ClickedStartButton()
         {
-            _uiPanel.SetActive(false);
+            _startCam.Priority = 0;
 
+            _startCam.enabled = false;
+            _freeLookCam.enabled = true;
+
+            _uiPanel.SetActive(false);
             Time.timeScale = 1.0f;
         }
 
