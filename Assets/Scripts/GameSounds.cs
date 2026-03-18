3885561d17c475aa845a9520b66f4216277983c3
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-03-18 18:26:07 +0900

    20260318_Commit

---
+++ Assets/Scripts/GameSounds.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace I_Walk
+{
+    public class GameSounds : MonoBehaviour
+    {
+        [SerializeField] GameObject _walkSound;
+        [SerializeField] GameObject _failSound;
+        [SerializeField] GameObject _gameMenuSound;
+        [SerializeField] GameObject _inGameBackgroundSound;
+        [SerializeField] GameObject _clickSound; //클릭하는 소리는 이거로 퉁치기
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
